729b0fccffe0deec11e201699e45d099a28b010c
Deepthi Reddy <deepthireddy4690@gmail.com>
2026-07-11 21:34:19 +0530

    Updated database for Render deployment

--- database/db.py
+++ database/db.py
@@ -1,11 +1,18 @@
+import os
 import psycopg2
 
+
 def get_connection():
-    connection = psycopg2.connect(
+
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    return psycopg2.connect(
         host="localhost",
         database="ai_expense_tracker",
         user="postgres",
         password="dfordatabase",
         port="5432"
     )
-    return connection
